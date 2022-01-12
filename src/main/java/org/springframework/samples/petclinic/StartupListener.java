74c8115752efda6874bf41862c84b3250c06b856
Gareth Evans <g.evans@sap.com>
2022-01-12 09:29:17 +0000

    Log the classpath for the app

--- src/main/java/org/springframework/samples/petclinic/StartupListener.java
+++ src/main/java/org/springframework/samples/petclinic/StartupListener.java
@@ -6,6 +6,7 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.lang.management.ManagementFactory;
 import java.util.Properties;
 
 @Component
@@ -26,6 +27,8 @@ public class StartupListener {
 		LOG.info("java.vendor:                " + p.getProperty("java.vendor"));
 		LOG.info("java.runtime.version:       " + p.getProperty("java.runtime.version"));
 		LOG.info("java.home:                  " + p.getProperty("java.home"));
+		LOG.info("========================================================================");
+		LOG.info("java.class.path:            " + System.getProperty("java.class.path"));
 		LOG.info("========================================================================");
 	}
 
